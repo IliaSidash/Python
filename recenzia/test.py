a604aa05ea96ccd89c2a888e883d08bd0bcf406d
IliaSidash <iliasidash@gmail.com>
2017-05-28 16:36:02 +0500

    del

--- recenzia/test.py
+++
@@ -1,6 +0,0 @@
-
-for count_analog in range(16, 32):
-    count_analog_board = round(count_analog/16)
-    print(count_analog / 16, count_analog_board)
-    print(count_analog // 16, count_analog_board)
-    print(count_analog % 16, count_analog_board, '\n')
